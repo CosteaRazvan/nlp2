3961808074ec5d6cbff57d23cc18fef7a262a652
Razvan Costea <rc2396@tobii.com>
2025-01-28 18:55:05 +0200

    comment print

--- training_dynamics/RoBERT-large-encoder_trained/V1_2024_10_31_19_58/train.py
+++ training_dynamics/RoBERT-large-encoder_trained/V1_2024_10_31_19_58/train.py
@@ -92,7 +92,7 @@ def train(config: ModelConfig, model, train_loader, val_loader, version, class_w
                 sample_label = labels[sample_id].item()
 
                 train_dynamics = {"guid": guid, f"logits_epoch_{epoch-1}": sample_logits, "gold": sample_label, "device": "cuda:0"}
-                print(train_dynamics)
+                # print(train_dynamics)
 
             # Gradient Accumulation Step
             if (k + 1) % config.gradient_accumulation == 0 or (k + 1) == len(train_loader):
